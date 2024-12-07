ac06c20b26f5fe8a724a0d87258bd969991319e9
OpenCo <admin@openco.ca>
2024-12-07 14:22:46 +0000

    Add passthrough copy for image files in Eleventy configuration

--- src/.eleventy.js
+++ src/.eleventy.js
@@ -1,10 +1,12 @@
-module.exports = function(eleventyConfig) {
-    // Copy `src/files` to `_site/files`
-    eleventyConfig.addPassthroughCopy({"files": "files"});
-    // Put robots.txt in root
-eleventyConfig.addPassthroughCopy({ 'robots.txt': 'robots.txt' });
-eleventyConfig.addPassthroughCopy({ '404.html':'404.html'}) 
-    return {
-      // other configuration...
-    };
+module.exports = function (eleventyConfig) {
+  // Copy `src/files` to `_site/files`
+  eleventyConfig.addPassthroughCopy({ "files": "files" });
+  // Put robots.txt in root
+  eleventyConfig.addPassthroughCopy({ 'robots.txt': 'robots.txt' });
+  eleventyConfig.addPassthroughCopy({ '404.html': '404.html' })
+  // Passthrough for jpg, jpeg, png, and svg files *.jpg, *.jpeg, *.png, *.svg no mater where they are
+  eleventyConfig.addPassthroughCopy('**/*.{jpg,jpeg,png,gif,svg,webp,ico}')
+  return {
+    // other configuration...
   };
+};
